b70c985f095066ded57694726af6b56c06a2e778
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-19 01:17:43 +0600

    add semantic

--- teebay-web-app/src/App.js
+++ teebay-web-app/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Teebay Web App
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
     </div>
   );
 }
