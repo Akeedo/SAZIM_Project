a09c098d6c0238f7f30d0e7c7ff47661999024a2
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-19 02:31:07 +0600

    Remove unnessary code

--- teebay-web-app/src/App.js
+++ teebay-web-app/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import CreateProduct from './components/product/create-product';
 
 function App() {
   return (
     <div className="main">
       <h2 className="main-header">React Crud Operations</h2>
+      <div>
+        <CreateProduct />
+      </div>
     </div>
   );
 }
