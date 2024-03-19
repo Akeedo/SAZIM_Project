9687090e254364e03741dde719b6a980cfc72500
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-20 00:01:35 +0600

    Add API function

--- teebay-web-app/src/components/product/get-all-product.js
+++ teebay-web-app/src/components/product/get-all-product.js
@@ -1,35 +1,21 @@
 
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import ProductTable from './product-table';
+import axios from 'axios';
 
 function GetAllProduct(){
-      // Sample products data
-  const sampleProducts = [
-        {
-            title: 'Ultra HD TV',
-            category: 'Electronics',
-            description: 'A large, ultra HD television, perfect for family movie nights.',
-            price: '999.99',
-        },
-        {
-            title: 'Bookshelf',
-            category: 'Furniture',
-            description: 'A wooden bookshelf with five shelves, ideal for any living room or study.',
-            price: '249.99',
-        },
-        {
-            title: 'Running Shoes',
-            category: 'Sportswear',
-            description: 'Lightweight running shoes with excellent grip and comfort.',
-            price: '79.99',
-        },
-    // Add more products as needed
-    ];
 
-    return (
+   const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        axios.get('https://65f88c14df151452460fa890.mockapi.io/api/v1/products')
+        .then((response) => {
+            setAPIData(response.data);
+        });
+    }, []);
+ return (
         <div>
             <h2>All Products</h2>
-            <ProductTable products={sampleProducts} />
+            <ProductTable products={APIData} />
         </div>
     );
 }
