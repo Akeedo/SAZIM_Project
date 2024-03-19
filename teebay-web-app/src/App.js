bb1ade45ff3e7adf56d9b6cc33f770441086b938
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-19 21:33:36 +0600

    Update routes in App.js

--- teebay-web-app/src/App.js
+++ teebay-web-app/src/App.js
@@ -1,23 +1,29 @@
 
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 import CreateProduct from './components/product/create-product';
+import GetAllProduct from './components/product/get-all-product';
 
 function App() {
   return (
-    <Router>
     <div className="main">
-      <h2 className="main-header">React Crud Operations</h2>
-      <div>
-        <Route exact path='/create-product' component={CreateProduct} />
-      </div>
-    </div>
+  <h2 className="main-header">React Crud Operations</h2>
+
+      <Router>
+      <Routes> 
+        
+        <Route path="/create-product" element={<CreateProduct />} />
+        <Route path="/get-all-product" element={<GetAllProduct />} />
+        {/* Add more routes as needed */}
+      </Routes>
     </Router>
+   </div>
+    
   );
 }
 
