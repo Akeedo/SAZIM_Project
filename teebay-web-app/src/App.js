18e9c3fc179c1b914248a3669cb5920cce8394bf
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-20 23:34:31 +0600

    feat(TASK-CODE): add CreateUser component and route for user creation functionality

--- teebay-web-app/src/App.js
+++ teebay-web-app/src/App.js
@@ -4,11 +4,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 import CreateProduct from './features/product/components/create-product';
 import GetAllProduct from './features/product/components/get-all-product';
 import UpdateProduct from './features/product/components/update-product';
+import CreateUser from './auth/user/components/create-user';
+
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
           <Route path="/create-product" element={<CreateProduct />} />
           <Route path="/" element={<GetAllProduct />} />
           <Route path="/update-product/:productId" element={<UpdateProduct />} />
+          <Route path="/create-user" element={<CreateUser />} />
         </Routes>
     </Router>
   </div>
