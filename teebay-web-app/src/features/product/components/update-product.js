cfa3dc37cbf83f027db2c6c83d8fe0a2ab774feb
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-20 16:58:39 +0600

    Add model and data service to update product

--- teebay-web-app/src/features/product/components/update-product.js
+++ teebay-web-app/src/features/product/components/update-product.js
@@ -2,100 +2,68 @@ import React, { useState, useEffect } from 'react';
 import { Button, Form, Input, TextArea, Dropdown } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import ProductDataService from '../services/product-data-service';
+import ProductModelService from '../services/product-model-service';
+import ProductForm from '../views/product-form';
 
 const UpdateProduct = () => {
   const { productId } = useParams(); 
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const initialProduct = ProductModelService.setDefaultProduct();
+
   const [product, setProduct] = useState({
-    title: '',
-    category: '',
-    description: '',
-    price: ''
+    title: initialProduct.title,
+    category: initialProduct.category,
+    description: initialProduct.description,
+    price: initialProduct.price
   });
 
-  const categoriesOptions = [
-    { key: 'electronics', value: 'electronics', text: 'Electronics' },
-    { key: 'furniture', value: 'furniture', text: 'Furniture' },
-    { key: 'home_appliances', value: 'home_appliances', text: 'Home Appliances' },
-    { key: 'sporting_goods', value: 'sporting_goods', text: 'Sporting Goods' },
-    { key: 'outdoor', value: 'outdoor', text: 'Outdoor' },
-    { key: 'toys', value: 'toys', text: 'Toys' },
-  ];
+  const categoriesOptions =  ProductModelService.getCategories();
 
   useEffect(() => {
     const fetchProduct = async () => {
-      setIsLoading(true);
-      try {
-        const response = await axios.get(`https://65f88c14df151452460fa890.mockapi.io/api/v1/products/${productId}`);
-        setProduct(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("There was an error fetching the product", error);
-        setError(error);
-        setIsLoading(false);
-      }
+        setIsLoading(true);
+        try {
+            const data = await ProductDataService.getProductById(productId);
+            setProduct(data);
+            setIsLoading(false);
+        } catch (error) {
+            console.error("There was an error fetching the product", error);
+            setError(error);
+            setIsLoading(false);
+        }
     };
 
     fetchProduct();
-  }, [productId]);
+}, [productId]); 
 
-  const handleChange = (e, { name, value }) => setProduct({ ...product, [name]: value });
+const handleChange = (e, { name, value }) => {
+  const updatedProductField = ProductModelService.updateProductField(product, name, value);
+  setProduct(updatedProductField);
+};
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.put(`https://65f88c14df151452460fa890.mockapi.io/api/v1/products/${productId}`, product);
-      console.log(response.data);
-      // Handle response or success side effects here
+        const updatedProduct = await ProductDataService.updateProduct(productId, product);
+        console.log(updatedProduct);
     } catch (error) {
-      console.error("There was an error updating the product", error);
-      setError(error);
+        console.error("There was an error updating the product", error);
+        setError(error);
     }
-  };
+};
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading product</div>;
 
   return (
-    <Form className="standard-form" onSubmit={handleSubmit}>
-      <Form.Field
-        control={Input}
-        label='Title'
-        placeholder='Title'
-        name='title'
-        value={product.title}
-        onChange={handleChange}
+    <ProductForm
+        product={product}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        categoriesOptions={categoriesOptions}
       />
-      <Form.Field
-        control={Dropdown}
-        label='Category'
-        placeholder='Category'
-        fluid
-        selection
-        options={categoriesOptions}
-        name='category'
-        value={product.category}
-        onChange={handleChange}
-      />
-      <Form.Field
-        control={TextArea}
-        label='Description'
-        placeholder='Description'
-        name='description'
-        value={product.description}
-        onChange={handleChange}
-      />
-      <Form.Field
-        control={Input}
-        label='Price'
-        placeholder='Price'
-        name='price'
-        value={product.price}
-        type='number'
-        onChange={handleChange}
-      />
-      <Button type='submit' loading={isLoading}>Update</Button>
-    </Form>
   );
 };
 
