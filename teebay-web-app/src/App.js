4288f543a11131fe98245915d21c32837bbf4f7e
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-20 15:39:04 +0600

    docs(TASK-CODE): correct import statements for product components in documentation

--- teebay-web-app/src/App.js
+++ teebay-web-app/src/App.js
@@ -6,9 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-import CreateProduct from './features/components/create-product';
-import GetAllProduct from './features/components/get-all-product';
-import UpdateProduct from './features/components/update-product';
+import CreateProduct from './features/product/components/create-product';
+import GetAllProduct from './features/product/components/get-all-product';
+import UpdateProduct from './features/product/components/update-product';
 
 function App() {
   return (
