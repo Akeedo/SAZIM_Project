153e13a568d1244675ca1e1fc1780c3e87a8244b
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-20 19:37:25 +0600

    feat(TASK-CODE): enhance GetAllProduct with navigation and product deletion functionality

--- teebay-web-app/src/features/product/components/get-all-product.js
+++ teebay-web-app/src/features/product/components/get-all-product.js
@@ -2,9 +2,16 @@
 import React, {useEffect, useState } from 'react';
 import ProductTable from '../views/product-table';
 import ProductDataService from '../services/product-data-service';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function GetAllProduct(){
+
+    const navigate = useNavigate();
+
+    const handleUpdateClick = (productId) => {
+        navigate(`/update-product/${productId}`);
+    };
 
     const [APIData, setAPIData] = useState([]);
    
@@ -21,11 +28,25 @@ function GetAllProduct(){
         getProducts();
     }, []); 
 
+    const onDelete = (id) => {
+        ProductDataService.deleteProduct(id)
+            .then(() => {
+                reload();
+            })
+            .catch(error => {
+                console.error("There was an error deleting the product", error);
+            });
+    };
+
+    const reload = () => {
+        window.location.reload();
+    }
+
 
     return (
         <div>
             <h2>All Products</h2>
-            <ProductTable products={APIData} />
+            <ProductTable products={APIData} onDelete={onDelete} handleUpdateClick={handleUpdateClick} />
         </div>
     );
 }
