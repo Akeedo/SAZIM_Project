9aa01b1cea62668c0166163bf2924162ed6a19e7
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-20 16:58:59 +0600

    Add all the methods for Product service

--- teebay-web-app/src/features/product/services/product-data-service.js
+++ teebay-web-app/src/features/product/services/product-data-service.js
@@ -12,8 +12,38 @@ const ProductDataService = () => {
         }
     };
 
+    const getAllProducts = async () => {
+      try {
+          const response = await axios.get(BASE_URL);
+          return response.data;
+      } catch (error) {
+          throw error;
+      }
+  };
+
+  const getProductById = async (productId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/${productId}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const updateProduct = async (productId, product) => {
+  try {
+      const response = await axios.put(`${BASE_URL}/${productId}`, product);
+      return response.data; // Returns the updated product data
+  } catch (error) {
+      throw error; // Rethrows the error to be caught by the caller
+  }
+};
+
     return {
         addProduct,
+        getAllProducts,
+        getProductById,
+        updateProduct
     };
 };
 
