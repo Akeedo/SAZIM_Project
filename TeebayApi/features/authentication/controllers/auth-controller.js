b1e24704cd3e545b14de95463fc39766155b817a
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-22 03:36:54 +0600

    feat(TASK-CODE): add AuthController for handling authentication-related operations

---
+++ TeebayApi/features/authentication/controllers/auth-controller.js
@@ -0,0 +1,10 @@
+// features/user/AuthController.js
+const AuthService = require('../services/auth-service');
+const jwt = require('jsonwebtoken');
+const JWTConfig = require('../../../config/JWTConfig');
+
+
+const AuthController = {
+};
+
+module.exports = AuthController;
