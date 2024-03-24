7c737eaf0108e69e13a9f02ed956c9449358b633
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-24 13:05:07 +0600

    feat(TASK-CODE): add routes for buying and renting products with authentication

--- TeebayApi/features/product/product-route.js
+++ TeebayApi/features/product/product-route.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const ProductController = require('./controller/product-controller');
-const { productValidationRules } = require('./validation/product-validation');
+const { productValidationRules, validateProduct } = require('./validation/product-validation');
 const authenticateToken = require('../../middlewire/authenticateToken');
 
 console.log('authToken', authenticateToken);
@@ -12,5 +12,7 @@ router.get('/', authenticateToken, ProductController.getProducts);
 router.get('/:id', authenticateToken, ProductController.getProductById);
 router.delete('/:id', authenticateToken, ProductController.deleteProduct);
 router.put('/:id', [ authenticateToken, productValidationRules(), ProductController.updateProduct]);
+router.post('/buy-product', [ authenticateToken, ProductController.buyProduct]);
+router.post('/rent-product', [ authenticateToken, ProductController.rentProduct]);
 
 module.exports = router;
