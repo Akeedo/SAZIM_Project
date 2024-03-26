631985823efc8f8ca2ac9293b8e440b220e892d6
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-27 03:21:43 +0600

    add typeDefs and resolvers in Apollo Server

--- TeebayApi/server.js
+++ TeebayApi/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+
 
 const cors = require('cors');
 const Joi = require('joi');
@@ -10,14 +10,15 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const ProductRoutes = require('./features/product/product-route');
 const AuthRoutes = require('./features/authentication/auth-route');
-const UserRoutes = require('./features/user/user-routes')
-
-const User = require('./features/product/models/User');
+const UserRoutes = require('./features/user/user-routes');
+const typeDefs = require('./config/graphql/schema/index')
+const { ApolloServer } = require('apollo-server');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use(cors());
+const prisma = new PrismaClient();
 
 // Joi schema definition
 const userSchema = Joi.object({
@@ -38,8 +39,53 @@ const validateUser = (req, res, next) => {
   app.use('/auth', AuthRoutes);
   app.use('/products', ProductRoutes);
   app.use('/users', UserRoutes);
-  
+
+const resolvers = {
+  Query: {
+    products: async () => {
+      return await prisma.products.findMany({
+        include: {
+          transactions: true, 
+        },
+      });
+    },
+    product: async (_, args) => {
+      const { id } = args;
+      return await prisma.products.findUnique({
+        where: { id: parseInt(id) },
+        include: {
+          transactions: true, 
+        },
+      });
+    },
+  },
+  Product: {
+    price: (parent) => parent.price.toNumber(),
+    transactions: (parent) => parent.transactions,
+  },
+  Transaction: {
+    date: (parent) => parent.date.toISOString(),
+    product: async (parent) => {
+      return await prisma.products.findUnique({
+        where: { id: parent.product_id },
+      });
+    },
+    user: async (parent) => {
+      return await prisma.users.findUnique({
+        where: { id: parent.user_id },
+      });
+    },
+  },
+};
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
