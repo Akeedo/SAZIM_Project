5ef03e65d455933073df4405ee3f0ecf82adf4c1
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-27 16:25:49 +0600

    feat(TASK-CODE): Add GraphQL type definitions for product, including queries and mutations.

---
+++ TeebayApi/features/product/schema/product-schema.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server');
+
+const productTypeDefs = gql`
+  type Product {
+    id: ID!
+    title: String!
+    description: String
+    price: Float!
+    category: String!
+    transactions: [Transaction] 
+  }
+
+  extend type Query {
+    products: [Product!]!
+    product(id: ID!): Product
+  }
+
+  extend type Mutation {
+    createProduct(title: String!, description: String, price: Float!, category: String!): Product
+    deleteProduct(id: ID!): Product
+    updateProduct(id: ID!, title: String, description: String, price: Float, category: String): Product 
+  }
+`;
+
+module.exports = productTypeDefs;
