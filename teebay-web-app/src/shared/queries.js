88c4762abe0f87d5f9d826729a7f1074ec67c952
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-27 21:31:54 +0600

    feat(TASK-CODE): Create GraphQL query definitions for application data fetching and mutations.

---
+++ teebay-web-app/src/shared/queries.js
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      title
+      description
+      price
+      category
+      transactions {
+        id
+        transaction_type
+        date
+        amount
+      }
+    }
+  }
+`;
+
+const GET_USER_TRANSACTIONS_WITH_PRODUCTS = gql`
+    query GetUserTransactionsWithProducts($userId: ID!) {
+        userTransactionsWithProducts(userId: $userId) {
+            transaction_type
+            product {
+                id
+                title
+                description
+                category
+                price
+            }
+        }
+    }
+`;
+
+export default { GET_PRODUCTS, GET_USER_TRANSACTIONS_WITH_PRODUCTS };
