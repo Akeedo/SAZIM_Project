21278d0ce92d5300ad6b1ba38fb76d73a542d94b
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-27 21:23:04 +0600

    feat(TASK-CODE): Integrate ApolloProvider and client in app entry for GraphQL support.

--- teebay-web-app/src/index.js
+++ teebay-web-app/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css'
+import { ApolloProvider } from '@apollo/client';
+import client from './config/apollo-client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
