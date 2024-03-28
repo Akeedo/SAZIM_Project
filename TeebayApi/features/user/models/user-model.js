90a1755499bdb4eea5480c2dbe46266ff4d543c7
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-28 06:38:21 +0600

    fix(TASK-CODE): Correct UserModel.create method to match Prisma schema and user data structure adjustments.

--- TeebayApi/features/user/models/user-model.js
+++ TeebayApi/features/user/models/user-model.js
@@ -10,10 +10,18 @@ const UserModel = {
     },
   
     async create(userData) {
-      return await prisma.user.create({
-          data: userData,
+      return await prisma.users.create({
+        data: {
+          first_name: userData.firstName,         
+          last_name: userData.lastName,           
+          address: userData.address,              
+          phone_number: userData.phoneNumber,     
+          email: userData.email,                  
+          password: userData.password,            
+        },
       });
-  },
+    }
+    
   };
   
   module.exports = UserModel;
