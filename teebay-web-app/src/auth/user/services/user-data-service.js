4a7bead673e24d54d2eb12418f907a9a26180ffa
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-28 06:24:50 +0600

    refactor(TASK-CODE): Update UserDataService to use axiosInstance and add method for user transactions.

--- teebay-web-app/src/auth/user/services/user-data-service.js
+++ teebay-web-app/src/auth/user/services/user-data-service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import axiosInstance from '../../../utils/axiosInstance';
 
 const UserDataService = () => {
-    const BASE_URL = 'https://65f88c14df151452460fa890.mockapi.io/api/v1/users';
+    const BASE_URL = 'http://localhost:3030/users';
 
     const addUser = async (user) => {
         try {
@@ -12,47 +13,19 @@ const UserDataService = () => {
         }
     };
 
-    const getAllUsers = async () => {
+    const getAllUserTransactions = async (userId) => {
         try {
-            const response = await axios.get(BASE_URL);
+            const response = await axiosInstance({ method: 'GET', url: `/users/${userId}/transactions` }).then((response) => response);
             return response.data;
         } catch (error) {
             throw error;
         }
     };
 
-    const getUserById = async (userId) => {
-        try {
-            const response = await axios.get(`${BASE_URL}/${userId}`);
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
-    };
-
-    const updateUser = async (userId, user) => {
-        try {
-            const response = await axios.put(`${BASE_URL}/${userId}`, user);
-            return response.data; // Returns the updated user data
-        } catch (error) {
-            throw error; // Rethrows the error to be caught by the caller
-        }
-    };
-
-    const deleteUser = async (userId) => {
-        try {
-            await axios.delete(`${BASE_URL}/${userId}`);
-        } catch (error) {
-            throw error;
-        }
-    };
 
     return {
         addUser,
-        getAllUsers,
-        getUserById,
-        updateUser,
-        deleteUser
+        getAllUserTransactions
     };
 };
 
