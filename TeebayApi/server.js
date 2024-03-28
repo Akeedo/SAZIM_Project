b681cf0c71abca3bbfb445bee51f7f7025e9610c
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-28 06:34:32 +0600

    refactor(TASK-CODE): Streamline server setup with ApolloServer integration and remove unused imports.

--- TeebayApi/server.js
+++ TeebayApi/server.js
@@ -1,53 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { PrismaClient } = require('@prisma/client');
 
 
 const cors = require('cors');
 const Joi = require('joi');
 const { body, validationResult } = require('express-validator');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const ProductRoutes = require('./features/product/product-route');
 const AuthRoutes = require('./features/authentication/auth-route');
 const UserRoutes = require('./features/user/user-routes');
-const typeDefs = require('./config/graphql/schema/index');
-const resolvers = require('./config/graphql/resolvers/index');
-const { ApolloServer } = require('apollo-server');
+
+const setupApolloServer = require('./config/graphql/graphql-config');
+
 
 const app = express();
+const server = setupApolloServer(app);
+
 app.use(bodyParser.json());
 
 app.use(cors());
-const prisma = new PrismaClient();
 
-// Joi schema definition
-const userSchema = Joi.object({
-    userName: Joi.string().min(3).max(20).required(),
-    email: Joi.string().email({ tlds: { allow: false } }).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  });
-
-// Validation middleware
-const validateUser = (req, res, next) => {
-    const { error } = userSchema.validate(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
-    next();
-  };
 
   app.use('/auth', AuthRoutes);
   app.use('/products', ProductRoutes);
   app.use('/users', UserRoutes);
 
-
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.applyMiddleware({ app, path: '/graphql' });
+  app.get('/api/products', (req, res) => {
+    res.json([{
+        id: 1,
+        title: "Modular Shelving Unit",
+        description: "Customizable modular shelving unit, perfect for living rooms or offices.",
+        price: 320.00,
+        category: "furniture"
+      }
+    ]);
+  });
+  
 
 const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+(async () => {
+  await setupApolloServer(app);
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})();
