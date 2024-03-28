963214165dd3a5ab8df9b8faed9f2e270de74a00
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-28 06:22:27 +0600

    refactor(TASK-CODE): Wrap route components with AuthRoute for authentication checks in App component routing.

--- teebay-web-app/src/App.js
+++ teebay-web-app/src/App.js
@@ -11,24 +11,25 @@ import CreateUser from './auth/user/components/create-user';
 import Login from './auth/login/components/login';
 import DetailProduct from './features/product/components/detail-product';
 import UserTransactionHistory from './auth/user/components/user-transaction-history';
+import AuthRoute from './auth/routes/auth-route';
 
 
 function App() {
   return (
     <div className="main">
-  <h2 className="main-header">React Crud Operations</h2>
+  <h2 className="main-header">Teebay</h2>
 
-      <Router>
-        <Routes> 
-          <Route path="/create-product" element={<CreateProduct />} />
-          <Route path="/" element={<GetAllProduct />} />
-          <Route path="/update-product/:productId" element={<UpdateProduct />} />
+  <Router>
+        <Routes>
+          <Route path="/create-product" element={<AuthRoute component={CreateProduct} />} />
+          <Route path="/" element={<AuthRoute component={GetAllProduct} />} />
+          <Route path="/update-product/:productId" element={<AuthRoute component={UpdateProduct} />} />
           <Route path="/create-user" element={<CreateUser />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/product-detail/:productId" element={<DetailProduct />} />
-          <Route path="/user-product-transaction" element={<UserTransactionHistory />} />
+          <Route path="/product-detail/:productId" element={<AuthRoute component={DetailProduct} />} />
+          <Route path="/user-product-transaction" element={<AuthRoute component={UserTransactionHistory} />} />
         </Routes>
-    </Router>
+      </Router>
   </div>
     
   );
