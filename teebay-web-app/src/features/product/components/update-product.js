02aaa5547d81ebd9d1dd63a55587837bc1b5d1c6
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-28 06:30:40 +0600

    refactor(TASK-CODE): Enhance UpdateProduct with form validation, error handling, and navigate after successful update.

--- teebay-web-app/src/features/product/components/update-product.js
+++ teebay-web-app/src/features/product/components/update-product.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Button, Form, Input, TextArea, Dropdown } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import ProductDataService from '../services/product-data-service';
 import ProductModelService from '../services/product-model-service';
 import ProductForm from '../views/product-form';
+
 
 const UpdateProduct = () => {
   const { productId } = useParams(); 
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const navigate = useNavigate();
+
   const initialProduct = ProductModelService.setDefaultProduct();
 
   const [product, setProduct] = useState({ ...initialProduct });
+  const [validationErrors, setValidationErrors] = useState({});
 
   const categoriesOptions =  ProductModelService.getCategories();
 
@@ -40,9 +45,15 @@ const handleChange = (e, { name, value }) => {
 };
 
   const handleSubmit = async () => {
+    const errors = ProductModelService.validateProduct(product);
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors); 
+      return;
+    }
+    setValidationErrors({});
     try {
-        const updatedProduct = await ProductDataService.updateProduct(productId, product);
-        console.log(updatedProduct);
+        const response = await ProductDataService.updateProduct(productId, product);
+        alert(response.message);
     } catch (error) {
         console.error("There was an error updating the product", error);
         setError(error);
@@ -58,6 +69,7 @@ const handleChange = (e, { name, value }) => {
         handleChange={handleChange}
         handleSubmit={handleSubmit}
         categoriesOptions={categoriesOptions}
+        validationErrors={validationErrors}
       />
   );
 };
